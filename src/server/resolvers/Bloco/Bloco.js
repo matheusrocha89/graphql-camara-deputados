482af378313e1f6d0d63b7e423bad694b3a0a08f
matheusrocha89 <matheus.rocha89@gmail.com>
2019-04-12 19:47:04 +0100

    Creating pagination for:
    - Bloco
    - Deputado
    - Evento
    - Legislatura
    - Orgao
    - Partido
    - Proposicao
    - Votacao

--- src/server/resolvers/Bloco/Bloco.js
+++ src/server/resolvers/Bloco/Bloco.js
@@ -1,12 +1,17 @@
 import querystring from 'query-string';
 
 import clientAPI from '../../clients/deputados-api';
+import { returnPagination } from '../../utils/pagination';
 
 const blocos = async (_, args) => {
   try {
     const query = querystring.stringify(args);
     const { data } = await clientAPI.get(`/blocos?${query}`);
-    return data.dados;
+    const pagination = returnPagination(data.links);
+    return {
+      pageInfo: pagination,
+      edges: data.dados.map(item => ({ node: item })),
+    };
   } catch (e) {
     throw new Error(e.message);
   }
--- src/server/resolvers/Votacao/Votacao.js
+++ src/server/resolvers/Votacao/Votacao.js
@@ -2,6 +2,7 @@ import querystring from 'query-string';
 import omit from 'lodash/omit';
 
 import clientAPI from '../../clients/deputados-api';
+import { returnPagination } from '../../utils/pagination';
 
 const votacao = async (_, { id }) => {
   try {
@@ -16,7 +17,11 @@ const votacaoVotos = async (_, args) => {
   try {
     const query = querystring.stringify(omit(args, ['id']));
     const { data } = await clientAPI.get(`/votacoes/${args.id}/votos?${query}`);
-    return data.dados;
+    const pagination = returnPagination(data.links);
+    return {
+      pageInfo: pagination,
+      edges: data.dados.map(item => ({ node: item })),
+    };
   } catch (e) {
     throw new Error(e.message);
   }
